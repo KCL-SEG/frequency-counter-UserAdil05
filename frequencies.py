0bddb22db46fe04748e8124cbfbd2f95f70f849c
UserAdil05 <k20056435@kcl.ac.uk>
2023-12-08 15:35:45 +0000

    testing

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,22 @@
-"""Frequencies function."""
-"""ENTER YOUR SOLUTION HERE!"""
-
 def frequencies(items):
-    frequencies = {}
-    # Your code goes heredddjjdjjdjdjd
-    return frequenciesh
+    # Initialize an empty dictionary to store frequencies
+    frequency_dict = {}
+
+    # Iterate over each item in the list
+    for item in items:
+        # Convert the item to a string (if it's not already a string)
+        item_str = str(item)
+
+        # If the item is already in the dictionary, increment its count
+        if item_str in frequency_dict:
+            frequency_dict[item_str] += 1
+        else:
+            # Otherwise, add the item to the dictionary with a count of 1
+            frequency_dict[item_str] = 1
+
+    return frequency_dict
+
+# Test the function with the provided examples
+test1 = frequencies(['a', 'a', 'b', 'b', 'b', 'c'])
+test2 = frequencies([100, 'Hello', '100', '100', 100])
+
